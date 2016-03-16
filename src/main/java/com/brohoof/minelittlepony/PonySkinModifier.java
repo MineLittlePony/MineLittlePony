e18fd90c22ee8cb88a657eaf2c6a4f5742144b83
Matthew Messinger <mattmess1221@gmail.com>
2016-03-16 00:41:42 -0400

    Fix wing textures

--- src/main/java/com/brohoof/minelittlepony/PonySkinModifier.java
+++ src/main/java/com/brohoof/minelittlepony/PonySkinModifier.java
@@ -11,7 +11,19 @@ public class PonySkinModifier implements ISkinModifier {
     @Override
     public void convertSkin(BufferedImage skin, Graphics dest) {
         int scale = skin.getWidth() / 64;
-        drawImage(dest, skin, scale, 64, 32, 56, 48, 56, 16, 64, 32);
+
+        //top, mirror
+        drawImage(dest, skin, scale, 60, 34, 58, 32, 58, 18, 60, 16);
+        //bottom, mirror
+        drawImage(dest, skin, scale, 62, 34, 60, 32, 60, 18, 62, 16);
+        //inside
+        drawImage(dest, skin, scale, 56, 34, 58, 48, 60, 18, 62, 32);
+        //front, mirror
+        drawImage(dest, skin, scale, 60, 48, 58, 34, 58, 32, 60, 18);
+        //outside
+        drawImage(dest, skin, scale, 60, 34, 62, 48, 56, 18, 58, 32);
+        //back
+        drawImage(dest, skin, scale, 62, 34, 64, 48, 62, 18, 64, 32);
     }
 
     private void drawImage(Graphics graphics, Image image, int scale, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2) {
