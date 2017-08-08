486f787db39669464c2dc378698296b01c4d2d20
Matthew Messinger <mattmess1221@gmail.com>
2017-08-08 16:34:09 -0400

    Try not changing the skin server. Might improve skin loading.

--- src/main/java/com/minelittlepony/MineLittlePony.java
+++ src/main/java/com/minelittlepony/MineLittlePony.java
@@ -84,10 +84,10 @@ public class MineLittlePony {
     void postInit(Minecraft minecraft) {
 
         HDSkinManager manager = HDSkinManager.INSTANCE;
-        manager.setSkinUrl(SKIN_SERVER_URL);
-        manager.setGatewayURL(GATEWAY_URL);
+//        manager.setSkinUrl(SKIN_SERVER_URL);
+//        manager.setGatewayURL(GATEWAY_URL);
         manager.addSkinModifier(new PonySkinModifier());
-        logger.info("Set MineLP skin server URL.");
+//        logger.info("Set MineLP skin server URL.");
 
         RenderManager rm = minecraft.getRenderManager();
         this.saveCurrentRenderers(rm);
