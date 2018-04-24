1718868ca48603465911f4bf336b9baae4b30562
Sollace <sollacea@gmail.com>
2018-04-24 21:35:42 +0200

    Format PonyModelConstants

--- src/main/java/com/minelittlepony/model/PonyModelConstants.java
+++ src/main/java/com/minelittlepony/model/PonyModelConstants.java
@@ -2,51 +2,64 @@ package com.minelittlepony.model;
 
 public interface PonyModelConstants {
 
-    float BODY_CENTRE_X = 0.0F;
-    float BODY_CENTRE_Y = 8.0F;
-    float BODY_CENTRE_Z = 6.0F;
-    float BODY_ROTATE_ANGLE_X_NOTSNEAK = 0.0F;
-    float BODY_ROTATE_ANGLE_X_SNEAK = 0.4F;
-    float BODY_ROTATE_ANGLE_X_RIDING = (float) (Math.PI * 3.8);
-    float BODY_RP_Y_NOTSNEAK = 0.0F;
-    float BODY_RP_Y_SNEAK = 7.0F;
-    float BODY_RP_Y_RIDING = 1;
-    float BODY_RP_Z_NOTSNEAK = 0.0F;
-    float BODY_RP_Z_SNEAK = -4.0F;
-    float BODY_RP_Z_RIDING = 4F;
-    float EXT_WING_ROTATE_ANGLE_X = 2.5F;
-    float FIRSTP_ARM_CENTRE_X = -1.0F;
-    float FIRSTP_ARM_CENTRE_Y = 4.0F;
-    float FIRSTP_ARM_CENTRE_Z = 0.0F;
-    float FRONT_LEG_RP_Y_NOTSNEAK = 8.0F;
-    float FRONT_LEG_RP_Y_SNEAK = 8.0F;
-    float HEAD_CENTRE_X = 0.0F;
-    float HEAD_CENTRE_Y = -1.0F;
-    float HEAD_CENTRE_Z = -2.0F;
-    float HEAD_RP_X = 0.0F;
-    float HEAD_RP_Y = 0.0F;
-    float HEAD_RP_Z = 0.0F;
-    float LEFT_WING_EXT_RP_X = 4.5F;
-    float LEFT_WING_EXT_RP_Y = 5.0F;
-    float LEFT_WING_EXT_RP_Z = 6.0F;
-    float LEFT_WING_ROTATE_ANGLE_Z_SNEAK = -6.0F;
-    float RIGHT_WING_EXT_RP_X = -4.5F;
-    float RIGHT_WING_EXT_RP_Y = 5.0F;
-    float RIGHT_WING_EXT_RP_Z = 6.0F;
-    float RIGHT_WING_ROTATE_ANGLE_Z_SNEAK = 6.0F;
-    float ROTATE_270 = 4.712F;
-    float ROTATE_90 = 1.571F;
-    float SNEAK_LEG_X_ROTATION_ADJUSTMENT = 0.4F;
-    float TAIL_RP_X = 0.0F;
-    float TAIL_RP_Y = 0.0F;
-    float TAIL_RP_Z = 0.0F;
-    float TAIL_RP_Z_NOTSNEAK = 14.0F;
-    float TAIL_RP_Z_SNEAK = 15.0F;
-    float THIRDP_ARM_CENTRE_X = 0.0F;
-    float THIRDP_ARM_CENTRE_Y = 10.0F;
-    float THIRDP_ARM_CENTRE_Z = 0.0F;
-    float WING_FOLDED_RP_Y = 13.0F;
-    float WING_FOLDED_RP_Z = -3.0F;
-    float NECK_ROT_X = 0.166F;
+    float
+        BODY_CENTRE_X = 0.0F,
+        BODY_CENTRE_Y = 8.0F,
+        BODY_CENTRE_Z = 6.0F,
 
+        BODY_ROTATE_ANGLE_X_NOTSNEAK = 0.0F,
+        BODY_ROTATE_ANGLE_X_SNEAK = 0.4F,
+        BODY_ROTATE_ANGLE_X_RIDING = (float) (Math.PI * 3.8),
+
+        BODY_RP_Y_NOTSNEAK = 0.0F,
+        BODY_RP_Y_SNEAK = 7.0F,
+        BODY_RP_Y_RIDING = 1,
+
+        BODY_RP_Z_NOTSNEAK = 0.0F,
+        BODY_RP_Z_SNEAK = -4.0F,
+        BODY_RP_Z_RIDING = 4F,
+
+        EXT_WING_ROTATE_ANGLE_X = 2.5F,
+
+        FIRSTP_ARM_CENTRE_X = -1.0F,
+        FIRSTP_ARM_CENTRE_Y = 4.0F,
+        FIRSTP_ARM_CENTRE_Z = 0.0F,
+
+        FRONT_LEG_RP_Y_NOTSNEAK = 8.0F,
+        FRONT_LEG_RP_Y_SNEAK = 8.0F,
+
+        HEAD_CENTRE_X = 0.0F,
+        HEAD_CENTRE_Y = -1.0F,
+        HEAD_CENTRE_Z = -2.0F,
+
+        HEAD_RP_X = 0.0F,
+        HEAD_RP_Y = 0.0F,
+        HEAD_RP_Z = 0.0F,
+
+        LEFT_WING_EXT_RP_X = 4.5F,
+        LEFT_WING_EXT_RP_Y = 5.0F,
+        LEFT_WING_EXT_RP_Z = 6.0F,
+
+        LEFT_WING_ROTATE_ANGLE_Z_SNEAK = -6.0F,
+
+        ROTATE_270 = 4.712F,
+        ROTATE_90 = 1.571F,
+
+        SNEAK_LEG_X_ROTATION_ADJUSTMENT = 0.4F,
+
+        TAIL_RP_X = 0.0F,
+        TAIL_RP_Y = 0.0F,
+        TAIL_RP_Z = 0.0F,
+
+        TAIL_RP_Z_NOTSNEAK = 14.0F,
+        TAIL_RP_Z_SNEAK = 15.0F,
+
+        THIRDP_ARM_CENTRE_X = 0.0F,
+        THIRDP_ARM_CENTRE_Y = 10.0F,
+        THIRDP_ARM_CENTRE_Z = 0.0F,
+
+        WING_FOLDED_RP_Y = 13.0F,
+        WING_FOLDED_RP_Z = -3.0F,
+
+        NECK_ROT_X = 0.166F;
 }
