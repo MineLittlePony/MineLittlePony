e5a453abd0591e54cef1d954933ff092402d8660
Sollace <sollacea@gmail.com>
2018-04-28 12:44:10 +0200

    Reformatting missed some tabs

--- src/main/java/com/minelittlepony/MissingRendererException.java
+++ src/main/java/com/minelittlepony/MissingRendererException.java
@@ -1,9 +1,9 @@
 package com.minelittlepony;
 
 public class MissingRendererException extends RuntimeException {
-	private static final long serialVersionUID = -6059469512902628663L;
+    private static final long serialVersionUID = -6059469512902628663L;
 
-	public MissingRendererException(Class<?> cl) {
+    public MissingRendererException(Class<?> cl) {
         super("Could not find a renderer for " + cl.getName() + ". This is a bug.");
     }
 }
