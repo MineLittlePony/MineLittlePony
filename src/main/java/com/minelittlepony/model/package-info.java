e4281116cd6d6af6a4f07a3669a3e1a62eb43169
Sollace <sollacea@gmail.com>
2018-04-30 17:12:08 +0200

    Remove these todos because I think they've been done now

--- src/main/java/com/minelittlepony/model/package-info.java
+++ src/main/java/com/minelittlepony/model/package-info.java
@@ -5,5 +5,3 @@ package com.minelittlepony.model;
 import mcp.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-// TODO: Class/Enum/Interface names are inconsistent.
--- src/main/java/com/minelittlepony/package-info.java
+++ src/main/java/com/minelittlepony/package-info.java
@@ -5,5 +5,3 @@ package com.minelittlepony;
 import mcp.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
-// TODO: Code style is inconsistent
