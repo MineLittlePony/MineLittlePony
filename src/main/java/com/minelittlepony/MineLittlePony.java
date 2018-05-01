9f429415b5729ff5d3f03d2568c2c92836d01d1a
Sollace <sollacea@gmail.com>
2018-05-01 19:30:47 +0200

    Eclipse pls, I told you to use spaces

--- src/main/java/com/minelittlepony/MineLittlePony.java
+++ src/main/java/com/minelittlepony/MineLittlePony.java
@@ -26,7 +26,7 @@ public class MineLittlePony {
     public static final String MOD_NAME = "Mine Little Pony";
     public static final String MOD_VERSION = "@VERSION@";
 
-	private static final String SKIN_SERVER_URL = "minelpskins.voxelmodpack.com";
+    private static final String SKIN_SERVER_URL = "minelpskins.voxelmodpack.com";
     private static final String GATEWAY_URL = "minelpskinmanager.voxelmodpack.com";
 
     private static final KeyBinding SETTINGS_GUI = new KeyBinding("Settings", Keyboard.KEY_F9, "Mine Little Pony");
