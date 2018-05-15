e56611c3fc6a0f5d22804426186734953e1dcb9e
Sollace <sollacea@gmail.com>
2018-05-15 16:25:11 +0200

    Docs!

--- src/main/java/com/minelittlepony/gui/IActionable.java
+++ src/main/java/com/minelittlepony/gui/IActionable.java
@@ -1,5 +1,12 @@
 package com.minelittlepony.gui;
 
+/**
+ * An element that can perform an action.
+ */
+@FunctionalInterface
 public interface IActionable {
+    /**
+     * Does whatever.
+     */
     void perform();
 }
