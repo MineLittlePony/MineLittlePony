94be304ceee5365ec70db4364e1295c126f1bdca
Sollace <sollacea@gmail.com>
2018-07-15 00:21:53 +0200

    You don't need to do this

--- src/hdskins/java/com/voxelmodpack/hdskins/mod/HDSkinsMod.java
+++
@@ -1,8 +0,0 @@
-package com.voxelmodpack.hdskins.mod;
-
-import com.mumfrey.liteloader.Configurable;
-import com.mumfrey.liteloader.InitCompleteListener;
-import com.mumfrey.liteloader.ViewportListener;
-
-public interface HDSkinsMod extends InitCompleteListener, ViewportListener, Configurable {
-}
