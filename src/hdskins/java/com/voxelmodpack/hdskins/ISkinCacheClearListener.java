65e1e8078118f0e5a47869749ab154065f876987
Sollace <sollacea@gmail.com>
2018-07-19 22:21:41 +0200

    Docs

--- src/hdskins/java/com/voxelmodpack/hdskins/ISkinCacheClearListener.java
+++ src/hdskins/java/com/voxelmodpack/hdskins/ISkinCacheClearListener.java
@@ -1,5 +1,8 @@
 package com.voxelmodpack.hdskins;
 
+/**
+ * Callback to perfom additional actions when the skin cache is cleared.
+ */
 @FunctionalInterface
 public interface ISkinCacheClearListener {
     boolean onSkinCacheCleared();
--- src/hdskins/java/com/voxelmodpack/hdskins/ISkinModifier.java
+++ src/hdskins/java/com/voxelmodpack/hdskins/ISkinModifier.java
@@ -3,8 +3,16 @@ package com.voxelmodpack.hdskins;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+/**
+ * Interface for mods to convert player skins before loading.
+ */
 @FunctionalInterface
 public interface ISkinModifier {
-
+    /**
+     * Called to convert the given skin.
+     * 
+     * @param skin  The skin being converted
+     * @param dest  Graphics for drawing onto the image
+     */
     void convertSkin(BufferedImage skin, Graphics dest);
 }
