a4f53b59fafbd0f522c2c61856522814c3c00b70
Sollace <sollacea@gmail.com>
2018-09-07 20:22:51 +0200

    Removed the clear cache button. (no longer needed)

--- src/hdskins/java/com/voxelmodpack/hdskins/gui/HDSkinsConfigPanel.java
+++ src/hdskins/java/com/voxelmodpack/hdskins/gui/HDSkinsConfigPanel.java
@@ -1,9 +1,7 @@
 package com.voxelmodpack.hdskins.gui;
 
-import com.minelittlepony.gui.Button;
 import com.minelittlepony.gui.Checkbox;
 import com.minelittlepony.gui.SettingsPanel;
-import com.voxelmodpack.hdskins.HDSkinManager;
 import com.voxelmodpack.hdskins.LiteModHDSkins;
 import com.voxelmodpack.hdskins.upload.GLWindow;
 
@@ -12,9 +10,6 @@ public class HDSkinsConfigPanel extends SettingsPanel {
     public void initGui() {
         final LiteModHDSkins mod = LiteModHDSkins.instance();
 
-        addButton(new Button(40, 70, 100, 20, "hdskins.options.cache", sender -> {
-            HDSkinManager.INSTANCE.clearSkinCache();
-        }));
         addButton(new Checkbox(40, 40, "hdskins.options.skindrops", mod.experimentalSkinDrop, checked -> {
             mod.experimentalSkinDrop = checked;
 
