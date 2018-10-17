485de98c6946d3bffcbdc0461606ba32e952c5e3
Sollace <sollacea@gmail.com>
2018-10-17 10:06:20 +0200

    Change bat pony's ears (again)

--- src/main/java/com/minelittlepony/model/player/ModelBatpony.java
+++ src/main/java/com/minelittlepony/model/player/ModelBatpony.java
@@ -41,13 +41,13 @@ public class ModelBatpony extends ModelEarthPony implements IModelPegasus {
     protected void initEars(PonyRenderer head, float yOffset, float stretch) {
         head.child()
             .tex(12, 16).box(-4, -6, 1, 2, 2, 2, stretch)  // right ear
-              .tex(0, 3).box(-4, -6.49F, 2.49F, 1, 1, 1, stretch)
-              .tex(0, 5).box(-3, -6.25F, 1.25F, 1, 1, 1, stretch);
+              .tex(0, 3).box(-3.5F, -6.49F, 1.001F, 1, 1, 1, stretch)
+              .tex(0, 5).box(-3, -6.49F, 2.001F, 1, 1, 1, stretch);
 
         head.child().flip()
             .tex(12, 16).box( 2, -6, 1, 2, 2, 2, stretch)  // left ear
-              .tex(0, 3).box( 3, -6.49F, 2.49F, 1, 1, 1, stretch)
-              .tex(0, 5).box( 2, -6.25F, 1.25F, 1, 1, 1, stretch);
+              .tex(0, 3).box( 2.5F, -6.49F, 1.001F, 1, 1, 1, stretch)
+              .tex(0, 5).box( 2, -6.49F, 2.001F, 1, 1, 1, stretch);
     }
 
     @Override
