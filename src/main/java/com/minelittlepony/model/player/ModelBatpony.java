bf8f3ef02a858d13c7a3feb524befb0892b9d24a
PolyacovYury <kuf-1@yandex.ru>
2018-11-06 02:09:22 +0300

    Batpony ear tufts: z-fighting on inner sides of ears fixed

--- src/main/java/com/minelittlepony/model/player/ModelBatpony.java
+++ src/main/java/com/minelittlepony/model/player/ModelBatpony.java
@@ -20,12 +20,12 @@ public class ModelBatpony extends ModelPegasus {
         head.child()
             .tex(12, 16).box(-4, -6, 1, 2, 2, 2, stretch)  // right ear
               .tex(0, 3).box(-3.5F, -6.49F, 1.001F, 1, 1, 1, stretch)
-              .tex(0, 5).box(-3, -6.49F, 2.001F, 1, 1, 1, stretch);
+              .tex(0, 5).box(-2.998F, -6.49F, 2.001F, 1, 1, 1, stretch);
 
         head.child().flip()
             .tex(12, 16).box( 2, -6, 1, 2, 2, 2, stretch)  // left ear
               .tex(0, 3).box( 2.5F, -6.49F, 1.001F, 1, 1, 1, stretch)
-              .tex(0, 5).box( 2, -6.49F, 2.001F, 1, 1, 1, stretch);
+              .tex(0, 5).box( 1.998F, -6.49F, 2.001F, 1, 1, 1, stretch);
     }
 
     @Override
