b3c21cd47db2bb24fa258aa94618b70c56da753a
Sollace <sollacea@gmail.com>
2019-03-27 09:24:23 +0200

    Remove some leftover HDSkins code

--- src/hdskinslitemod/java/com/minelittlepony/hdskins/litemod/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.minelittlepony.hdskins.litemod;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
