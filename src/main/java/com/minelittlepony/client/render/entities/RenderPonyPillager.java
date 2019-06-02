91f445e1fafa5412172682aec7371ee02d02e722
Sollace <sollacea@gmail.com>
2019-06-02 17:23:06 +0200

    Add TODOs with things to be done

---
+++ src/main/java/com/minelittlepony/client/render/entities/RenderPonyPillager.java
@@ -0,0 +1,5 @@
+package com.minelittlepony.client.render.entities;
+
+public class RenderPonyPillager {
+    // TODO: Render Pillagers as changelings
+}
---
+++ src/main/java/com/minelittlepony/client/render/entities/RenderPonyRavager.java
@@ -0,0 +1,5 @@
+package com.minelittlepony.client.render.entities;
+
+public class RenderPonyRavager {
+    // TODO: Render the Ravager as Lord Tirek
+}
