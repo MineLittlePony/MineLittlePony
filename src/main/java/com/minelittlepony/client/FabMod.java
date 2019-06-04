e26eb0961632a77296e242499828ae7c5a3f4b36
Sollace <sollacea@gmail.com>
2019-06-04 17:06:14 +0200

    Moved the KeyBindings boilerplate to Kirin

--- src/main/java/com/minelittlepony/client/FabMod.java
+++ src/main/java/com/minelittlepony/client/FabMod.java
@@ -1,15 +1,9 @@
 package com.minelittlepony.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
-
 import com.minelittlepony.client.gui.hdskins.MineLPHDSkins;
 import com.minelittlepony.common.client.IModUtilities;
 
@@ -46,21 +40,5 @@ public class FabMod implements ClientModInitializer, ClientTickCallback, IModUti
         } else {
             mlp.onTick(client, client.world != null && client.player != null);
         }
-    }
-
-    @Override
-    public KeyBinding registerKeybind(String category, int key, String bindName) {
-        // normalize Fabric's behavior
-        if (bindName.startsWith("key.")) {
-            bindName = bindName.replace("key.", "");
-        }
-
-        FabricKeyBinding binding = FabricKeyBinding.Builder.create(new Identifier(bindName) {
-            @Override
-            public String toString() { return getPath(); }
-        }, InputUtil.Type.KEYSYM, key, category).build();
-
-        KeyBindingRegistry.INSTANCE.register(binding);
-        return binding;
     }
 }
