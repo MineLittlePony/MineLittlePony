b290d4c7e7e3c03c9b5a8d4c7243dd9859ebe97e
Sollace <sollacea@gmail.com>
2019-06-26 15:43:31 +0200

    Fixed offset on swimming ponies

--- src/main/java/com/minelittlepony/client/transform/PostureSwimming.java
+++ src/main/java/com/minelittlepony/client/transform/PostureSwimming.java
@@ -19,7 +19,7 @@ public class PostureSwimming extends PostureFlight {
     protected double calculateIncline(PlayerEntity player, double motionX, double motionY, double motionZ) {
         double motionLerp = MathUtil.clampLimit(Math.sqrt(motionX * motionX + motionZ * motionZ) * 30, 1) / 2;
 
-        GlStateManager.translated(0, 0, -1);
+        GlStateManager.translated(0, 0.9, -1);
 
         return 90 + super.calculateIncline(player, motionX, motionY, motionZ) * motionLerp;
     }
