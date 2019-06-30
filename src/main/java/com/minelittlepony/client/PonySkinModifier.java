2f38419bc2350ccfbeab1a811b2b40912476ad40
Sollace <sollacea@gmail.com>
2019-06-30 12:11:42 +0200

    Convert the old Graphics calls into NativeImage calls (should fix legacy skins)

--- src/main/java/com/minelittlepony/client/PonySkinModifier.java
+++ src/main/java/com/minelittlepony/client/PonySkinModifier.java
@@ -5,16 +5,18 @@ import com.minelittlepony.hdskins.ISkinModifier;
 public class PonySkinModifier implements ISkinModifier {
 
     @Override
-    public void convertSkin(ISkinModifier.IDrawer drawer) {
+    public void convertSkin(IDrawer drawer) {
+       // copies the wing across for old legacy textures.
+
+       // Scale factor so we can support texture sizes beyond 64x64
        int scale = drawer.getImage().getWidth() / 64;
 
-       // TODO: What are these numbers!?
-
-       drawer.draw(scale, 60, 32, 58, 34, 58, 16, 60, 18, false, false); // top, mirror
-       drawer.draw(scale, 62, 32, 60, 34, 60, 16, 62, 18, false, false); // bottom, mirror
-       drawer.draw(scale, 58, 34, 56, 48, 60, 18, 62, 32, false, false); // inside
-       drawer.draw(scale, 60, 34, 58, 48, 58, 18, 60, 32, false, false); // back
-       drawer.draw(scale, 62, 34, 60, 48, 56, 18, 58, 32, false, false); // outside
-       drawer.draw(scale, 64, 34, 62, 48, 62, 18, 64, 32, false, false); // back
+                      // ( from ) ( to ) (size) flipX flipY
+       drawer.draw(scale, 58, 16, 58, 32, 2,  2, true, false); // top
+       drawer.draw(scale, 60, 16, 60, 32, 2,  2, true, false); // bottom
+       drawer.draw(scale, 60, 18, 56, 34, 2, 14, true, false); // inside
+       drawer.draw(scale, 58, 18, 58, 34, 2, 14, true, false); // back
+       drawer.draw(scale, 56, 18, 60, 34, 2, 14, true, false); // outside
+       drawer.draw(scale, 62, 18, 62, 34, 2, 14, true, false); // front
     }
 }
