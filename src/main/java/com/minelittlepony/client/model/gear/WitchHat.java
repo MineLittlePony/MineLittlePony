b879ac2f44994ff0ed74ec94f8c334612004b622
Sollace <sollacea@gmail.com>
2019-11-28 14:56:47 +0200

    Convert the witch hat to json

--- src/main/java/com/minelittlepony/client/model/gear/WitchHat.java
+++ src/main/java/com/minelittlepony/client/model/gear/WitchHat.java
@@ -1,10 +1,8 @@
 package com.minelittlepony.client.model.gear;
 
-import net.minecraft.client.model.ModelPart;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
-import com.minelittlepony.client.util.render.Part;
 import com.minelittlepony.model.BodyPart;
 import com.minelittlepony.model.IModel;
 import com.minelittlepony.model.gear.IStackable;
@@ -15,33 +13,10 @@ public class WitchHat extends AbstractGear implements IStackable {
 
     private static final Identifier WITCH_TEXTURES = new Identifier("textures/entity/witch.png");
 
-    private ModelPart witchHat;
-
-    public WitchHat() {
-        init(0, 0);
-    }
-
     @Override
     public void init(ModelContext context) {
-        witchHat = context.findByName("witch_hat");
+        accept(context.findByName("witch_hat"));
     }
-
-    @Deprecated
-    public void init(float yOffset, float stretch) {
-        accept(witchHat = new Part(this).size(64, 128));
-        ((Part)witchHat).around(HEAD_RP_X, HEAD_RP_Y + yOffset, HEAD_RP_Z)
-                .tex(0, 64).box(-5, -6, -7, 10, 2, 10, stretch)
-                .child(0).around(1.75F, -4, 2)
-                    .tex(0, 76).box(-5, -5, -7, 7, 4, 7, stretch)
-                    .rotate(-0.05235988F, 0, 0.02617994F)
-                    .child(0).around(1.75F, -4, 2)
-                        .tex(0, 87).box(-5, -4, -7, 4, 4, 4, stretch)
-                        .rotate(-0.10471976F, 0, 0.05235988F)
-                        .child(0).around(1.75F, -2, 2)
-                            .tex(0, 95).box(-5, -2, -7, 1, 2, 1, stretch)
-                            .rotate(-0.20943952F, 0, 0.10471976F);
-    }
-
 
     @Override
     public boolean canRender(IModel model, Entity entity) {
