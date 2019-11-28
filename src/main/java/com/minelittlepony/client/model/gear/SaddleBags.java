db6a1f68e1fc30566c0c61da40c93e08bb03918a
Sollace <sollacea@gmail.com>
2019-11-28 14:36:55 +0200

    Convert saddlebags to json

--- src/main/java/com/minelittlepony/client/model/gear/SaddleBags.java
+++ src/main/java/com/minelittlepony/client/model/gear/SaddleBags.java
@@ -1,6 +1,5 @@
 package com.minelittlepony.client.model.gear;
 
-import com.minelittlepony.client.util.render.Part;
 import com.minelittlepony.model.BodyPart;
 import com.minelittlepony.model.IModel;
 import com.minelittlepony.model.IPegasus;
@@ -30,63 +29,13 @@ public class SaddleBags extends AbstractGear {
 
     float dropAmount = 0;
 
-
     private IModel model;
-
-    public SaddleBags() {
-        init(0, 0);
-    }
 
     @Override
     public void init(ModelContext context) {
         strap = context.findByName("strap");
         leftBag = context.findByName("left_bag");
         rightBag = context.findByName("right_bag");
-    }
-
-    @Deprecated
-    public void init(float yOffset, float stretch) {
-        leftBag = new Part(this, 56, 19);
-        rightBag = new Part(this, 56, 19);
-        strap = new Part(this, 56, 19);
-
-        float y = -0.5F;
-        int x = 4;
-        int z = -1;
-
-        ((Part)strap).offset(-x, y + 0.2F, z + 3).around(0, 4, 4)
-        .tex(56, 31).top(0, 0, 0, 8, 1, stretch)
-                    .top(0, 0, 1, 8, 1, stretch)
-                  .south(0, 0, 2, 8, 1, stretch)
-                  .north(0, 0, 0, 8, 1, stretch)
-                .child(0).offset(0, -3, -0.305F).tex(56, 31)
-                   .west( 4.0002F,  0, 0, 1, 3, stretch)  // 0.0001 is there
-                   .west( 4.0002F, -1, 0, 1, 3, stretch)  // otherwise straps
-                   .west(-4.0002F,  0, 0, 1, 3, stretch)  // clip into the body
-                   .west(-4.0002F, -1, 0, 1, 3, stretch)
-                .pitch = ROTATE_270;
-
-        ((Part)leftBag).offset(x, y, z).around(0, 4, 4)
-                .tex(56, 25).south(0, 0, 0, 3, 6, stretch)
-                .tex(59, 25).south(0, 0, 8, 3, 6, stretch)
-                .tex(56, 19) .west(3, 0, 0, 6, 8, stretch)
-                             .west(0, 0, 0, 6, 8, stretch)
-                .child(0).offset(z, y, -x).tex(56, 16)
-                                     .top(0, 0, -3, 8, 3, stretch)
-              .tex(56, 22).flipZ().bottom(0, 6, -3, 8, 3, stretch)
-                         .yaw = ROTATE_270;
-
-        x += 3;
-
-        ((Part)rightBag).offset(-x, y, z).around(0, 4, 4).flipX()
-                .tex(56, 25).south(0, 0, 0, 3, 6, stretch)
-                .tex(59, 25).south(0, 0, 8, 3, 6, stretch)
-                .tex(56, 19).west(3, 0, 0, 6, 8, stretch)
-                            .west(0, 0, 0, 6, 8, stretch)
-                   .child(0).offset(z, y, x).tex(56, 16)
-                            .flipZ().top(0, 0, -3, 8, 3, stretch)
-             .tex(56, 22).flipZ().bottom(0, 6, -3, 8, 3, stretch)
-                 .yaw = ROTATE_270;
     }
 
     @Override
