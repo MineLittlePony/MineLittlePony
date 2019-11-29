dcd2442442bd85cba988f69e973de304c2c5afc9
Sollace <sollacea@gmail.com>
2019-11-29 11:30:41 +0200

    Tidy up the magic glow layer

--- src/main/java/com/minelittlepony/client/render/MagicGlow.java
+++ src/main/java/com/minelittlepony/client/render/MagicGlow.java
@@ -5,17 +5,18 @@ import net.minecraft.client.render.RenderPhase;
 import net.minecraft.client.render.VertexFormats;
 
 public class MagicGlow extends RenderPhase {
-
-    public MagicGlow(String name, Runnable beginAction, Runnable endAction) {
+    private MagicGlow(String name, Runnable beginAction, Runnable endAction) {
         super(name, beginAction, endAction);
     }
 
+    static final RenderLayer MAGIC = RenderLayer.method_24048("mlp_magic_glow", VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL, 7, 256, RenderLayer.MultiPhaseData.builder()
+            .texture(NO_TEXTURE)
+            .transparency(TRANSLUCENT_TRANSPARENCY)
+            .lightmap(DISABLE_LIGHTMAP)
+            .cull(DISABLE_CULLING)
+            .build(false));
+
     public static RenderLayer getRenderLayer() {
-        return RenderLayer.method_24048("mlp_magic_glow", VertexFormats.POSITION_COLOR_TEXTURE_LIGHT_NORMAL, 7, 256, RenderLayer.MultiPhaseData.builder()
-                .texture(NO_TEXTURE)
-                .transparency(TRANSLUCENT_TRANSPARENCY)
-                .lightmap(DISABLE_LIGHTMAP)
-                .cull(DISABLE_CULLING)
-                .build(false));
+        return MAGIC;
     }
 }
