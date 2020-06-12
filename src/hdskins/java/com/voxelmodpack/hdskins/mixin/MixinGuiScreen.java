6cd6e5e3b18ebcab743de265b63f07352921cb14
Sollace <sollacea@gmail.com>
2020-06-13 00:19:15 +0200

    Fix and bump

--- src/hdskins/java/com/voxelmodpack/hdskins/mixin/MixinGuiScreen.java
+++ src/hdskins/java/com/voxelmodpack/hdskins/mixin/MixinGuiScreen.java
@@ -9,7 +9,7 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiYesNoCallback;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,11 +17,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GuiScreen.class)
 abstract class MixinGuiScreen extends Gui implements GuiYesNoCallback {
 
-    @Shadow
-    protected abstract <T extends GuiButton> T addButton(T buttonIn);
+    @Invoker("addButton")
+    protected abstract <T extends GuiButton> T invokeAddButton(T buttonIn);
 
     @Inject(method = "setWorldAndResolution(Lnet/minecraft/client/Minecraft;II)V", at = @At("RETURN"))
     private void setWorldAndResolution(Minecraft mc, int width, int height, CallbackInfo info) {
-        HDSkinManager.INSTANCE.displaySkinningGui((GuiScreen)(Object)this, this::addButton);
+        HDSkinManager.INSTANCE.displaySkinningGui((GuiScreen)(Object)this, this::invokeAddButton);
     }
 }
