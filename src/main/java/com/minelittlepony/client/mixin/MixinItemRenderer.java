83e253448f7d490c1e17d481be617141fcd58cfa
Sollace <sollacea@gmail.com>
2021-02-26 16:18:08 +0200

    Fix mixins

--- src/main/java/com/minelittlepony/client/mixin/MixinItemRenderer.java
+++ src/main/java/com/minelittlepony/client/mixin/MixinItemRenderer.java
@@ -20,14 +20,14 @@ abstract class MixinItemRenderer {
 
     private static final String Boolean = "Z";
 
-    @Inject(method = "getArmorVertexConsumer(" + VertexConsumerProvider + RenderLayer + Boolean + Boolean + ")" + VertexConsumer, at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getArmorGlintConsumer(" + VertexConsumerProvider + RenderLayer + Boolean + Boolean + ")" + VertexConsumer, at = @At("HEAD"), cancellable = true)
     private static void onGetArmorVertexConsumer(VertexConsumerProvider provider, RenderLayer layer, boolean solid, boolean glint, CallbackInfoReturnable<VertexConsumer> info) {
         if (LevitatingItemRenderer.usesTransparency()) {
             info.setReturnValue(provider.getBuffer(LevitatingItemRenderer.getRenderLayer()));
         }
     }
 
-    @Inject(method = "getDirectGlintVertexConsumer(" + VertexConsumerProvider + RenderLayer + Boolean + Boolean + ")" + VertexConsumer, at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getDirectItemGlintConsumer(" + VertexConsumerProvider + RenderLayer + Boolean + Boolean + ")" + VertexConsumer, at = @At("HEAD"), cancellable = true)
     private static void onMethod_29711(VertexConsumerProvider provider, RenderLayer layer, boolean solide, boolean glint, CallbackInfoReturnable<VertexConsumer> info) {
         if (LevitatingItemRenderer.usesTransparency()) {
             info.setReturnValue(provider.getBuffer(LevitatingItemRenderer.getRenderLayer()));
