e31ee1c53b8d2617f84bb571d89fffdb203e9b4e
Sollace <sollacea@gmail.com>
2023-10-05 20:28:29 +0100

    Fixed tiny silly ponies reverting to normal villagers when they grow up

--- src/main/java/com/minelittlepony/client/render/entity/npc/textures/SillyPonyTextureSupplier.java
+++ src/main/java/com/minelittlepony/client/render/entity/npc/textures/SillyPonyTextureSupplier.java
@@ -8,7 +8,9 @@ public class SillyPonyTextureSupplier {
     public static <T extends LivingEntity & VillagerDataContainer> TextureSupplier<T> create(TextureSupplier<T> fallback, TextureSupplier<String> formatter) {
         Identifier egg = formatter.apply("silly_pony");
         Identifier egg2 = formatter.apply("tiny_silly_pony");
-        return entity -> isBestPony(entity) ? (entity.isBaby() ? egg2 : egg) : fallback.apply(entity);
+        return entity -> {
+            return isBestPony(entity) ? ("Dinky".equals(entity.getCustomName().getString()) ? egg2 : egg) : fallback.apply(entity);
+        };
     }
 
     public static boolean isBestPony(LivingEntity entity) {
@@ -16,7 +18,7 @@ public class SillyPonyTextureSupplier {
             return false;
         }
         String name = entity.getCustomName().getString();
-        return "Derpy".equals(name) || (entity.isBaby() && "Dinky".equals(name));
+        return "Derpy".equals(name) || "Dinky".equals(name);
     }
 
     public static boolean isCrownPony(LivingEntity entity) {
