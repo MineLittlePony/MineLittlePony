0198838d78caa03890a318d76bb35a9b52425fa9
Sollace <sollacea@gmail.com>
2025-11-04 02:40:56 +0000

    Fixed fillycam causing the camera to clip the ground in certain scenarios

--- src/main/java/com/minelittlepony/client/mixin/MixinCamera.java
+++ src/main/java/com/minelittlepony/client/mixin/MixinCamera.java
@@ -1,25 +1,41 @@
 package com.minelittlepony.client.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.minelittlepony.api.pony.Pony;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
 @Mixin(Camera.class)
 abstract class MixinCamera {
-    @ModifyReturnValue(method = "clipToSpace(F)F", at = @At("RETURN"))
-    private float redirectCameraDistance(float value) {
-        if (MinecraftClient.getInstance().player != null) {
-            Pony pony = Pony.getManager().getPony(MinecraftClient.getInstance().player);
+    @Shadow
+    private Entity focusedEntity;
 
-            if (!pony.race().isHuman()) {
-                value *= pony.size().eyeDistanceFactor();
-            }
+    @ModifyArg(method = "update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V", at = @At(value = "INVOKE", target = "java/lang/Math.max(FF)F"), index = 0)
+    private float adjustCameraDistance(float value) {
+        return value * minelp_getDistanceScale(focusedEntity);
+    }
+
+    @ModifyArg(method = "update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V", at = @At(value = "INVOKE", target = "java/lang/Math.max(FF)F"), index = 1)
+    private float adjustVehicleCameraDistance(float value) {
+        if (focusedEntity.hasVehicle() && focusedEntity.getVehicle() instanceof LivingEntity l) {
+            return value * minelp_getDistanceScale(l);
         }
-        return value;
+        return value * minelp_getDistanceScale(focusedEntity);
+    }
+
+    @Unique
+    private float minelp_getDistanceScale(Entity entity) {
+        Pony pony = Pony.getManager().getPony(entity).orElse(null);
+
+        if (pony != null && !pony.race().isHuman()) {
+            return pony.size().eyeDistanceFactor();
+        }
+
+        return 1;
     }
 }
