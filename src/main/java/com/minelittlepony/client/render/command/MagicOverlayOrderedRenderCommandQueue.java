a45834580fa2839594c9292640997c20d33f9b10
Sollace <sollacea@gmail.com>
2026-04-24 10:40:14 +0100

    Fix crash rendering deadmau5 player head with magic overlay

--- src/main/java/com/minelittlepony/client/render/command/MagicOverlayOrderedRenderCommandQueue.java
+++ src/main/java/com/minelittlepony/client/render/command/MagicOverlayOrderedRenderCommandQueue.java
@@ -1,6 +1,6 @@
 package com.minelittlepony.client.render.command;
 
-import it.unimi.dsi.fastutil.objects.Object2ObjectAVLTreeMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.client.renderer.OrderedSubmitNodeCollector;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.rendertype.RenderType;
@@ -8,10 +8,11 @@ import net.minecraft.client.renderer.rendertype.RenderType;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public class MagicOverlayOrderedRenderCommandQueue extends MagicOverlayRenderCommandQueue implements SubmitNodeCollector {
-    private final Object2ObjectAVLTreeMap<OrderedSubmitNodeCollector, MagicOverlayRenderCommandQueue> queues = new Object2ObjectAVLTreeMap<>();
+    private final Map<OrderedSubmitNodeCollector, MagicOverlayRenderCommandQueue> queues = new Object2ObjectOpenHashMap<>();
 
     public MagicOverlayOrderedRenderCommandQueue(SubmitNodeCollector parent, Function<RenderType, @Nullable RenderType> layer, int color, List<Pass> passes) {
         super(parent, parent, layer, color, passes);
